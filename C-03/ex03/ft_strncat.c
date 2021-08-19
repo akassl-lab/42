caf5153b6b792f9312173af09722da3149c51c53
akassl <nmallett@c1r8p9.42quebec.com>
2021-08-19 15:41:34 -0400

    Removed unused var c02/ex03

--- C-03/ex03/ft_strncat.c
+++ C-03/ex03/ft_strncat.c
@@ -6,14 +6,13 @@
 /*   By: nmallett <nmallett@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/19 11:55:33 by nmallett          #+#    #+#             */
-/*   Updated: 2021/08/19 12:05:52 by nmallett         ###   ########.fr       */
+/*   Updated: 2021/08/19 15:40:45 by nmallett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
 	unsigned int		i[2];
-	unsigned int		nb2;
 
 	i[0] = 0;
 	i[1] = 0;
@@ -21,7 +20,7 @@ char	*ft_strncat(char *dest, char *src, unsigned int nb)
 	{
 		i[0]++;
 	}
-	while (src[i[1]] != '\0' && i[1] <= nb2)
+	while (src[i[1]] != '\0' && i[1] <= nb)
 	{
 		dest[i[0] + i[1]] = src[i[1]];
 		i[1]++;
