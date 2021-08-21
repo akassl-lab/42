42e3537e0cfa89cd6079c8ab4e2b89bf9ae591a1
akassl <nmallett@c1r1p4.42quebec.com>
2021-08-21 13:01:51 -0400

    WIP

--- Rush02/ex00/main.c
+++ Rush02/ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: nmallett <nmallett@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 19:13:18 by nmallett          #+#    #+#             */
-/*   Updated: 2021/08/20 19:13:22 by nmallett         ###   ########.fr       */
+/*   Updated: 2021/08/21 13:01:24 by nmallett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,11 @@
 
 #define MAX_BUFFER_SIZE 1096
 
+//char fetchedValue[];
+
 int main(int argc, char **argv)
 {
+
     if (argc < 2)
     {
         ft_putchar("Vous devez entrer au minimum un argument\n");
@@ -36,6 +39,39 @@ int main(int argc, char **argv)
     }
     else
     {
+
+		/* Testing Part */
+		
+		int iEx[2] = {0, ft_atoi(argv[1])};
+		int iLoop = 0;
+
+	
+		while (argv[1][iLoop] != '\0' && argv[1][iLoop] >= 48 && argv[1][iLoop] <= 57 )
+			iLoop++;
+
+
+
+		while (iLoop >= 0)
+		{
+			iLoop--;
+			printf("%d\n", ft_split2numbers(iLoop, ft_atoi(argv[1])));
+			if (iLoop == 0)
+				break;
+		}
+		
+
+
+
+		/*
+		while (iEx <= ft_atoi(argv[iEx]))
+		{
+			//printf("%d\n", ft_split2numbers(1, ft_atoi(argv[iEx])));
+			iEx++;
+		}
+*/
+		return 0;
+		
+
         char *buff = (char*) malloc (MAX_BUFFER_SIZE);
         int file = open("utils/numbers.dict", O_RDONLY);
 
@@ -54,12 +90,15 @@ int main(int argc, char **argv)
         {           
             if(ft_atoi(argv[1]) == ft_atoi(&buff[i]))
             {
-                int i2 = i;
+                int i2;
+				
+				i2 = 0;
                 while(buff[i2++] != '\n')
                 {
                     if (buff[i2] != ':' && buff[i2] != ' ' && buff[i2] >= 97 && buff[i2] <= 122)
-                        write(1, &buff[i2], 1);
-                    
+					{
+						write(1, &buff[i2], 1);
+					}
                 }
 
 
--- Rush02/ex00/utils/utils.c
+++ Rush02/ex00/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: nmallett <nmallett@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 19:13:18 by nmallett          #+#    #+#             */
-/*   Updated: 2021/08/20 19:13:22 by nmallett         ###   ########.fr       */
+/*   Updated: 2021/08/21 13:01:17 by nmallett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ extern void     ft_putchar(char *str);
 extern int      ft_isdigit(char *str);
 extern int      ft_atoi(char *str);
 extern char	    *ft_strstr(char *str, char *to_find);
+extern int		*t_ultimate_div_mod(int *a, int *b);
+extern int		ft_split2numbers(int a, int b);
 
 void    ft_putchar(char *str)
 {
@@ -46,7 +48,7 @@ int ft_atoi(char *str)
     
     if(*str == '-') 
     {
-        i[2] = -1;
+        i[1] = -1;
         str++;
     }
     
@@ -56,7 +58,7 @@ int ft_atoi(char *str)
       i[0] = i[0] + (*str - 48);
       str++;
     }
-    return (i[2] * i[0]);
+    return (i[1] * i[0]);
 }
 
 char	*ft_strstr(char *str, char *to_find)
@@ -77,4 +79,18 @@ char	*ft_strstr(char *str, char *to_find)
 		istr++;
 	}	
 	return (0);
+}
+
+int		*ft_ultimate_div_mod(int *a, int *b)
+{
+	*a = *a / *b;
+	*b = *a % *b;
+	return(b);
+}
+
+int ft_split2numbers(int a, int b)
+{
+    while(a--)
+        b/=10;
+    return b%10;
 }
