578a766d22465f49c2197ab999cda6b81ba5fbfb
akassl <nmallett@c1r8p9.42quebec.com>
2021-08-23 14:13:36 -0400

    Modification to the main

--- BSQ/main.c
+++ BSQ/main.c
@@ -6,17 +6,27 @@
 /*   By: nmallett <nmallett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 12:20:03 by nmallett          #+#    #+#             */
-/*   Updated: 2021/08/23 12:41:46 by nmallett         ###   ########.fr       */
+/*   Updated: 2021/08/23 14:13:13 by nmallett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <fcntl.h>
+
+#include <sys/wait.h>
+
 #include "includes/libs.c"
 
-#ifndef DEV_MODE
-    #define MAX_BUFFER_SIZE 3096
+void    terminateProcess(char *buffer, unsigned int flag);
+
+#ifdef DEV_MODE
+    #define MAX_BUFFER_SIZE     3096
+    #define TERMINATE_WAIT      5
 #else
-    #define MAX_BUFFER_SIZE 1024
+    #define MAX_BUFFER_SIZE     1024
+    #define TERMINATE_WAIT      5
 #endif
 
 
@@ -25,8 +35,46 @@ int main(int argsc, char **argv)
     if (argsc < 2) {
         ft_putstr("Vous devez utiliser au minimum un argument"); return (0);
     }
+    else if(argsc > 2) {
+        ft_putstr("Vous devez maximum un arguement"); return (0);
+    }
+    else
+        #define FILE_TO_OPEN argv[1]
 
 
-    printf("testing");
+    #ifdef FILE_TO_OPEN
+        printf("File we are using %s\n", FILE_TO_OPEN);
+
+        // Let's create the buffer using the buffer size we defined and allocate an address
+        // To open the file
+
+        char *buffer = (char*) malloc (MAX_BUFFER_SIZE);
+
+        int file = open(FILE_TO_OPEN, O_RDONLY);
+
+        if (file == -1) {
+            // Put flag to 1 to ignore buffer since we did not use it yet so we need to be able to pass our condition even if the buffer is free
+            // But we still need to free the allocated address that we requested
+            terminateProcess(buffer, 1);
+        }
+    #else
+        printf("An error occured");
+        return (0);
+    #endif
+
     return (0);
+}
+
+// Flag 0 = Using the buffer or flag 1 = Ignore the buffer
+void    terminateProcess(char *buffer, unsigned int flag)
+{
+    if(*buffer || flag == 1)
+    {
+        free(buffer); // Free the allocated address
+        #undef FILE_TO_OPEN // Undef our FILE_TO_OPEN
+        printf("Terminating process in %d second(s)", TERMINATE_WAIT); // Give it a small delai to let the buffer free completely
+        exit(TERMINATE_WAIT); // Terminate the process
+    }
+    else
+        printf("Could not terminate process, the buffer was invalid");
 }
