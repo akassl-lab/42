1930305d6b5c98421068d466b4a134f7c521dfa3
akassl <76759193+akassl-lab@users.noreply.github.com>
2021-10-06 16:38:25 -0400

    Fixed Makefile

--- 42cursus/libft/src/main.c
+++ 42cursus/libft/src/main.c
@@ -6,15 +6,16 @@
 /*   By: nmallett <nmallett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 12:00:37 by nmallett          #+#    #+#             */
-/*   Updated: 2021/10/06 14:03:45 by nmallett         ###   ########.fr       */
+/*   Updated: 2021/10/06 15:55:54 by nmallett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 int main()
 {
- 	ft_putendl_fd("test", 0);
-	ft_putendl_fd("test", 0);
+	ft_putstr("test");
+ 	//printf("%d", ft_isalpha("a"));
  	return 0;
 }
