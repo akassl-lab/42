3ab069c5313997503be9b9a47e680dc710841619
akassl <76759193+akassl-lab@users.noreply.github.com>
2021-10-06 16:37:21 -0400

    Fix ft_strlen

--- 42cursus/libft/src/ft_strlen.c
+++ 42cursus/libft/src/ft_strlen.c
@@ -6,18 +6,20 @@
 /*   By: nmallett <nmallett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 16:53:14 by nmallett          #+#    #+#             */
-/*   Updated: 2021/10/06 14:02:17 by nmallett         ###   ########.fr       */
+/*   Updated: 2021/10/06 16:16:34 by nmallett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int    ft_strlen(char *str)
+int    ft_strlen(const char *str)
 {
     int     i;
+	char	*string;
 
     i = 0;
-    while (str[i] && str[i] != '\0')
+	string = (char *) str;
+    while (string[i] && string[i] != '\0')
          i++;
     return (i);
 }
