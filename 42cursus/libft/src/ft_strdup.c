320b09c23cd1d49ae5e60905ec960d2aa733a9fb
akassl <76759193+akassl-lab@users.noreply.github.com>
2021-10-08 17:21:34 -0400

    Fix ft_strdup

--- 42cursus/libft/src/ft_strdup.c
+++ 42cursus/libft/src/ft_strdup.c
@@ -6,24 +6,25 @@
 /*   By: nmallett <nmallett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 16:53:02 by nmallett          #+#    #+#             */
-/*   Updated: 2021/10/06 14:02:35 by nmallett         ###   ########.fr       */
+/*   Updated: 2021/10/08 15:52:40 by nmallett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strdup(char *s)
+char    *ft_strdup(const char *s)
 {
     int     i;
     char    *ptr;
-    
-    ptr = (char *) ft_calloc(ft_strlen(s), sizeof(char *));
-
-    i = 0;
+	
+	i = 0;
+	if (!(ptr = ft_calloc(ft_strlen(s)+1, sizeof(char))))
+		return (NULL);
     while (s[i] != '\0')
     {
         ptr[i] = s[i];
-        i++;
+		i++;
     }
+	ptr[++i] = '\0';
     return(ptr);
 }
