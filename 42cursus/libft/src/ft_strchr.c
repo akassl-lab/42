e97781b1b467dd93cf0ac40ab2f6ea896499004f
akassl <76759193+akassl-lab@users.noreply.github.com>
2021-10-08 17:21:25 -0400

    Fix ft_strchr

--- 42cursus/libft/src/ft_strchr.c
+++ 42cursus/libft/src/ft_strchr.c
@@ -6,26 +6,24 @@
 /*   By: nmallett <nmallett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 16:52:58 by nmallett          #+#    #+#             */
-/*   Updated: 2021/10/06 16:33:24 by nmallett         ###   ########.fr       */
+/*   Updated: 2021/10/08 12:11:19 by nmallett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strchr(const char *str, int n)
+char	*ft_strchr(const char *s, int n)
 {
-    size_t      i;
-    char    *newStr;
+	char	*str;
 
-    i = 0;
-    newStr      = (char *) str;
-
-    while (newStr[i] != '\0')
-    {
-        if (newStr[i] == n)
-            return (&newStr[i]);
-        i++;
-    }
-    return (0);
+	if (s == NULL)
+		return (NULL);
+	str = (char *)s;
+	while (*str != (char) n)
+	{
+		if (*str == '\0')
+			return (NULL);
+		str++;
+	}
+	return (str);
 }
-
