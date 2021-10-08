858e252bcf5e4a52a1ebb6159e12e65408413c4b
akassl <76759193+akassl-lab@users.noreply.github.com>
2021-10-08 17:22:03 -0400

    Fix ft_strlcpy

--- 42cursus/libft/src/ft_strlcpy.c
+++ 42cursus/libft/src/ft_strlcpy.c
@@ -6,23 +6,31 @@
 /*   By: nmallett <nmallett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 16:53:11 by nmallett          #+#    #+#             */
-/*   Updated: 2021/10/06 14:02:19 by nmallett         ###   ########.fr       */
+/*   Updated: 2021/10/08 14:33:46 by nmallett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t  ft_strlcpy(char *dest, const char *src, size_t size)
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-    size_t    srcLength;
-    char    *destStr;
-    char    *srcStr;
-    destStr = (char *)  dest;
-    srcStr = (char *)  src;
+	size_t	i;
 
-    srcLength = ft_strlen(srcStr);
-
-    if (srcLength < size)
-        ft_memcpy(destStr, srcStr, srcLength);
-    return srcLength;
+	i = 0;
+	if (size == 0)
+	{
+		while (src[i])
+			i++;
+		return (i);
+	}
+	while (i < size - 1 && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < size)
+		dest[i] = '\0';
+	while (src[i] != '\0')
+		i++;
+	return (i);
 }
