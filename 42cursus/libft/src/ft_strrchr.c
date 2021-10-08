7447a0049ce1a5d59b7544eebebc45d149ba560c
akassl <76759193+akassl-lab@users.noreply.github.com>
2021-10-08 17:22:24 -0400

    Fix ft_strrchr

--- 42cursus/libft/src/ft_strrchr.c
+++ 42cursus/libft/src/ft_strrchr.c
@@ -6,24 +6,28 @@
 /*   By: nmallett <nmallett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 16:53:28 by nmallett          #+#    #+#             */
-/*   Updated: 2021/10/06 16:33:47 by nmallett         ###   ########.fr       */
+/*   Updated: 2021/10/08 12:29:12 by nmallett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strrchr(const char *str, int n)
+char	*ft_strrchr(const char *str, int n)
 {
-    size_t      i;
-    char    *newStr;
+	unsigned int	i;
+	char	*strEx;
+	char	n2;
 
-    newStr      = (char *) str;
-    i           = ft_strlen(newStr);
-    while (i >= 0)
-    {
-        if (newStr[i] == n)
-            return (&newStr[i]);
-        i--;
-    }
-    return (0);
+	strEx = NULL;
+	n2 = (char) n;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == n2)
+			strEx = (char *)&str[i];
+		i++;
+	}
+	if (n2 == str[i])
+		return ((char *)&str[i]);
+	return (strEx);
 }
