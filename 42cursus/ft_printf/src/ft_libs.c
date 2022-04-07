7d858570b0d72cce82b71c2fe56ff0816c2e6fd3
akassl <76759193+akassl-lab@users.noreply.github.com>
2022-04-07 13:49:33 -0400

    Updated latest version of ft_printf

---
+++ 42cursus/ft_printf/src/ft_libs.c
@@ -0,0 +1,80 @@
+
+# include "ft_printf.h"
+
+
+    int     ft_putnbr(int n)
+    {
+        int i;
+
+        i = 0;
+        if (n == -2147483648)
+            i += ft_putstr("-2147483648");
+        else if (n < 0)
+        {
+            i += ft_putchar('-');
+            i += ft_putnbr(-n);
+        }
+        else if (n <= 9)
+            i += ft_putchar(n + '0');
+        else
+        {
+            i += ft_putnbr(n / 10);
+            i += ft_putnbr(n % 10);
+        }
+        return(i);
+    }
+
+    int 	ft_putnbr_unsigned(unsigned int n)
+    {
+        int     i;
+
+        i = 0;
+        if (n < 0)
+        {
+            i += ft_putchar('-');
+            i += ft_putnbr_unsigned(-n);
+        }
+        else if (n <= 9)
+            i += ft_putchar(n + '0');
+        else
+        {
+            i += ft_putnbr_unsigned(n / 10);
+            i += ft_putnbr_unsigned(n % 10);
+        }
+        return(i);
+    }
+
+    int 	ft_put_hex(unsigned long long hex, const int type)
+    {
+        int i;
+
+        i = 0;
+        if (hex >= 16)
+        {
+            i += ft_put_hex(hex / 16, type);
+            i += ft_put_hex(hex % 16, type);
+        }
+        else
+        {
+            if (hex <= 9)
+                i += ft_putchar((hex + 48));
+            else
+                if (type == 1)
+                    i += ft_putchar((hex - 10 + 97));
+                else if (type == 2)
+                    i += ft_putchar((hex - 10 + 65));
+        }
+        return (i);
+    }
+
+    int 	ft_put_addr(void *ptr)
+    {
+        uintptr_t	ptr2;
+        int 		c;
+
+        c = 0;
+        ptr2 = (uintptr_t)ptr;
+        c += ft_putstr("0x");
+        c += ft_put_hex(ptr2, 1);
+        return (c);
+    }
---
+++ 42cursus/ft_printf/src/ft_printf.h
@@ -0,0 +1,24 @@
+#ifndef FT_PRINTF_H
+# include <stdarg.h>
+# include <stdio.h>
+# include <unistd.h>
+# define FT_PRINTF_H
+
+    typedef struct t_struct
+    {
+        int	i;
+        int	str;
+        va_list	args;
+    }	t_struct;
+
+    int			ft_printf(const char *str, ...);
+    void        switch_cases(const char *str, t_struct *stack);
+    int 		str_len(int ptr);
+    int 		ft_putchar(char c);
+    int 		ft_putstr(char *str);
+    int 		ft_putnbr(int n);
+    int 		ft_putnbr_unsigned(unsigned int n);
+    int 		ft_put_hex(unsigned long long hex, const int type);
+    int         ft_put_ptr(int ptr);
+    int         ft_put_addr(void *ptr);
+#endif
