14c63f5a962b02be2a8f8568895ab29d10d10df9
akassl <76759193+akassl-lab@users.noreply.github.com>
2022-04-07 14:12:33 -0400

    Update ft_printf.h

--- 42cursus/ft_printf/src/ft_printf.h
+++ 42cursus/ft_printf/src/ft_printf.h
@@ -1,8 +1,8 @@
-#ifndef FT_PRINTF_H
+#ifndef	FT_PRINTF_H
+#define FT_PRINTF_H
 # include <stdarg.h>
 # include <stdio.h>
 # include <unistd.h>
-# define FT_PRINTF_H
 
     typedef struct t_struct
     {
@@ -11,14 +11,15 @@
         va_list	args;
     }	t_struct;
 
-    int			ft_printf(const char *str, ...);
-    void        switch_cases(const char *str, t_struct *stack);
-    int 		str_len(int ptr);
-    int 		ft_putchar(char c);
-    int 		ft_putstr(char *str);
-    int 		ft_putnbr(int n);
-    int 		ft_putnbr_unsigned(unsigned int n);
-    int 		ft_put_hex(unsigned long long hex, const int type);
-    int         ft_put_ptr(int ptr);
-    int         ft_put_addr(void *ptr);
+int		ft_printf(const char *str, ...);
+void	switch_cases(const char *str, t_struct *stack);
+int		str_len(int ptr);
+int		ft_putchar(char c);
+int		ft_putstr(char *str);
+int		ft_putnbr(int n);
+int		ft_putnbr_unsigned(unsigned int n);
+int		ft_put_hex(unsigned long long hex, const int type);
+int		ft_put_ptr(int ptr);
+int		ft_put_addr(void *ptr);
+
 #endif
