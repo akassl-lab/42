92bca2626bdd76fb347a44061921a60d10770817
akassl <76759193+akassl-lab@users.noreply.github.com>
2022-05-04 14:34:04 -0400

    Updated libft

--- 42cursus/libft/src/src/ft_putnbr.c
+++ 42cursus/libft/src/src/ft_putnbr.c
@@ -1,25 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_put_addr.c                                      :+:      :+:    :+:   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nmallett <nmallett@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/26 12:00:26 by nmallett          #+#    #+#             */
-/*   Updated: 2022/05/04 14:31:41 by nmallett         ###   ########.fr       */
+/*   Created: 2022/04/28 13:41:11 by nmallett          #+#    #+#             */
+/*   Updated: 2022/05/04 14:31:47 by nmallett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-int	ft_put_addr(void *ptr)
+int	ft_putnbr(int n)
 {
-	uintptr_t	ptr2;
-	int			c;
+	int	i;
 
-	c = 0;
-	ptr2 = (uintptr_t)ptr;
-	c += ft_putstr("0x");
-	c += ft_put_hex(ptr2, 1);
-	return (c);
+	i = 0;
+	if (n == -2147483648)
+		i += ft_putstr("-2147483648");
+	else if (n < 0)
+	{
+		i += ft_putchar('-');
+		i += ft_putnbr(-n);
+	}
+	else if (n <= 9)
+		i += ft_putchar(n + '0');
+	else
+	{
+		i += ft_putnbr(n / 10);
+		i += ft_putnbr(n % 10);
+	}
+	return (i);
 }
